799211ab6ebdf84cbdb1368151cc8a1769478301
Pamu-Wick <pamudithawm@gmail.com>
2023-07-01 13:36:29 +0530

    added validTill

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,11 @@
 let checkInterval = setInterval(function() {
     const peakElement = document.querySelector('li.slide.selected .used-of');
     const fullElement = document.querySelector('li.slide:not(.selected) .used-of');
+    const validTillElement = document.querySelector('li.slide.selected .text-center.blue');
 
-    if (!peakElement || !fullElement) {
+    //validTillElement <p className="text-center blue">(Valid Till : 31-Jul)</p>
+
+    if (!peakElement || !fullElement || !validTillElement) {
         console.log('One or more elements could not be found, retrying...');
     } else {
         clearInterval(checkInterval);  // Stop checking when the elements are found
@@ -13,7 +16,11 @@ let checkInterval = setInterval(function() {
         let fullText = fullElement.textContent;
         let [fullUsed, fullTotal] = fullText.split(" USED OF ").map(parseFloat);
 
-        console.log(peakUsed, peakTotal, fullUsed, fullTotal);
+        let validTillText = validTillElement.textContent;
+        let validTill = validTillText.split(" : ")[1].split(")")[0];
+
+
+        console.log(peakUsed, peakTotal, fullUsed, fullTotal, validTill);
     }
 }, 1000);  // Check every 1000 milliseconds = 1 second
- 
+
