3c7f46e8751372a5e2ef2d72d4d549cf198c997e
Pamu-Wick <pamudithawm@gmail.com>
2023-07-23 15:13:25 +0530

    added calculateUsage

---
+++ calculateUsage.js
@@ -0,0 +1,98 @@
+export function calculateUsage(usageSummery) {
+
+    console.log("calculateUsage function called")
+    let usageSummeryJson = JSON.parse(usageSummery);
+
+    const peak = usageSummeryJson.dataBundle.my_package_info.usageDetails[0];
+    const standard = usageSummeryJson.dataBundle.my_package_info.usageDetails[1];
+
+    const peakRemain = peak.remaining;
+    const peakUsed = peak.used;
+    const peakLimit = peak.limit;
+    const peakPercentage = peak.percentage;
+
+    const fullRemain = standard.remaining;
+    const fullUsed = standard.used;
+    const fullLimit = standard.limit;
+    const fullPercentage = standard.percentage;
+
+    const offPeakTotal = fullLimit - peakLimit;
+    const offPeakUsed = (fullUsed - peakUsed).toFixed(1);
+    const offPeakRemain = (offPeakTotal - offPeakUsed).toFixed(1);
+    const offPeakPercentage = Math.round((offPeakUsed / offPeakTotal) * 100);
+
+    const validTill = peak.expiry_date;
+
+    // const calculateRemainPercent = (remain, total) => Math.round((remain / total) * 100);
+    const calculateDailyQuota = (total) => (total / 30).toFixed(1);
+
+    const daysCount = (validTill) => {
+        const [day, month] = validTill.split('-');
+        const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
+            'Oct', 'Nov', 'Dec'];
+        const monthNumber = monthNames.indexOf(month) + 1;
+        const currentYear = new Date().getFullYear();
+        const validTillDate = new Date(`${currentYear}-${monthNumber}-${day}`);
+        validTillDate.setHours(0, 0, 0, 0);  // set time to the start of the day
+
+        const currentDate = new Date();
+        currentDate.setHours(0, 0, 0, 0);  // set time to the start of the day
+
+        const startDate = new Date(validTillDate); // set date to the start of the current month
+        startDate.setMonth(startDate.getMonth() - 1);
+
+        const differenceInTime = validTillDate.getTime() - currentDate.getTime();
+        const differenceInDays = differenceInTime / (1000 * 3600 * 24); // remaining days
+
+        const pastTime = currentDate.getTime() - startDate.getTime();
+        const pastDays = pastTime / (1000 * 3600 * 24) + 1;
+
+        return {
+            passedDays: Math.ceil(pastDays),
+            remainingDays: Math.ceil(differenceInDays)
+        };
+    }
+
+    const dc = daysCount(validTill);
+    const remainingDays = dc.remainingDays;
+    const passedDays = dc.passedDays;
+
+    const dailyQuota = (data, days) => {
+        return (data / days).toFixed(1);
+    }
+
+    return {
+        peak: {
+            used: peakUsed,
+            total: peakLimit,
+            remain: peakRemain,
+            percentage: 100 - peakPercentage,
+            dailyQuota: calculateDailyQuota(peakLimit),
+            currentDailyQuota: dailyQuota(peakUsed, passedDays),
+            remainDailyQuota: dailyQuota(peakRemain, remainingDays)
+        },
+        offPeak: {
+            used: offPeakUsed,
+            total: offPeakTotal,
+            remain: offPeakRemain,
+            percentage: offPeakPercentage,
+            dailyQuota: calculateDailyQuota(offPeakTotal),
+            currentDailyQuota: dailyQuota(offPeakUsed, passedDays),
+            remainDailyQuota: dailyQuota(offPeakRemain, remainingDays)
+        },
+        total: {
+            used: fullUsed,
+            total: fullLimit,
+            remain: fullRemain,
+            percentage: 100 - fullPercentage,
+            dailyQuota: calculateDailyQuota(fullLimit),
+            currentDailyQuota: dailyQuota(fullUsed, passedDays),
+            remainDailyQuota: dailyQuota(fullLimit - fullUsed, remainingDays)
+        },
+        validTill: validTill,
+        passedDays: passedDays,
+        remainDays: remainingDays,
+
+    };
+
+}
--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
-import {daysCount} from './utils/daysCount.js';
+import {calculateUsage} from './calculateUsage.js';
+
 
 const uid = localStorage.getItem('uid')
 const pwd = localStorage.getItem('pwd')
@@ -73,8 +74,8 @@ async function getAccountDetails(accessToken, uid) {
         "body": null,
         "method": "GET"
     }).then(res => res.json()
-        ).then(data => {
-            localStorage.setItem('telephoneNo', data.dataBundle[0].telephoneno);
+    ).then(data => {
+        localStorage.setItem('telephoneNo', data.dataBundle[0].telephoneno);
         return data.dataBundle[0].telephoneno;
     }).then(telephoneNo => {
         getServiceDetails(accessToken, telephoneNo)
@@ -104,14 +105,18 @@ async function getServiceDetails(accessToken, telephoneNo) {
         "method": "GET"
     })
         .then(res => res.json()
-            .then(data => {
-                localStorage.setItem('serviceID', data.dataBundle.listofBBService[0].serviceID);
-            })
-        )
+        ).then(data => {
+        localStorage.setItem('serviceID', data.dataBundle.listofBBService[0].serviceID);
+    }).then(() => {
+            sid = localStorage.getItem('serviceID');
+            accTkn = localStorage.getItem('accessToken');
+            getUsageSummery(sid, accTkn);
+        }
+    )
     ;
 }
 
-async function getUsageSummery(serviceID,accessToken) {
+async function getUsageSummery(serviceID, accessToken) {
 
     console.log("getUsageSummery function called")
     fetch(`https://omniscapp.slt.lk/mobitelint/slt/api/BBVAS/UsageSummary?subscriberID=${serviceID}`, {
@@ -134,79 +139,9 @@ async function getUsageSummery(serviceID,accessToken) {
     }).then(
         res => res.json()
     ).then(data => {
-        data = JSON.stringify(data);
-        console.log(calculateUsage(data));
+            data = JSON.stringify(data);
+            console.log(calculateUsage(data));
         }
-    )}
-
-function calculateUsage(usageSummery) {
-
-    console.log("calculateUsage function called")
-    let usageSummeryJson = JSON.parse(usageSummery);
-
-    const peak = usageSummeryJson.dataBundle.my_package_info.usageDetails[0];
-    const standard = usageSummeryJson.dataBundle.my_package_info.usageDetails[1];
-
-    const peakRemain = peak.remaining;
-    const peakUsed = peak.used;
-    const peakLimit = peak.limit;
-    const peakPercentage = peak.percentage;
-
-    const fullRemain = standard.remaining;
-    const fullUsed = standard.used;
-    const fullLimit = standard.limit;
-    const fullPercentage = standard.percentage;
-
-    const offPeakTotal = fullLimit - peakLimit;
-    const offPeakUsed = (fullUsed - peakUsed).toFixed(1);
-    const offPeakRemain = (offPeakTotal - offPeakUsed).toFixed(1);
-    const offPeakPercentage = Math.round((offPeakUsed / offPeakTotal) * 100);
-
-    const validTill = peak.expiry_date;
-
-    // const calculateRemainPercent = (remain, total) => Math.round((remain / total) * 100);
-    const calculateDailyQuota = (total) => (total / 30).toFixed(1);
-
-    const dc = daysCount(validTill);
-    const remainingDays = dc.remainingDays;
-    const passedDays = dc.passedDays;
-
-    const dailyQuota = (data, days) => {
-        return (data / days).toFixed(1);
-    }
-
-    return {
-        peak: {
-            used: peakUsed,
-            total: peakLimit,
-            remain: peakRemain,
-            percentage: 100 - peakPercentage,
-            dailyQuota: calculateDailyQuota(peakLimit),
-            currentDailyQuota: dailyQuota(peakUsed, passedDays),
-            remainDailyQuota: dailyQuota(peakRemain, remainingDays)
-        },
-        offPeak: {
-            used: offPeakUsed,
-            total: offPeakTotal,
-            remain: offPeakRemain,
-            percentage: offPeakPercentage,
-            dailyQuota: calculateDailyQuota(offPeakTotal),
-            currentDailyQuota: dailyQuota(offPeakUsed, passedDays),
-            remainDailyQuota: dailyQuota(offPeakRemain, remainingDays)
-        },
-        total: {
-            used: fullUsed,
-            total: fullLimit,
-            remain: fullRemain,
-            percentage: 100 - fullPercentage,
-            dailyQuota: calculateDailyQuota(fullLimit),
-            currentDailyQuota: dailyQuota(fullUsed, passedDays),
-            remainDailyQuota: dailyQuota(fullLimit - fullUsed, remainingDays)
-        },
-        validTill: validTill,
-        passedDays: passedDays,
-        remainDays: remainingDays,
-
-    };
-
+    )
 }
+
