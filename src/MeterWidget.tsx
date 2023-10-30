40e819c847f1b0f180cda38795d2645e84714145
Pamu_Wick <pamudithawm@gmail.com>
2023-10-30 22:30:59 +0530

    add flowbite-react

--- src/MeterWidget.tsx
+++ src/MeterWidget.tsx
@@ -1,8 +1,8 @@
+import {Alert} from 'flowbite-react';
+import React, { Component } from "react";
 function MeterWidget() {
     return (
-        <div>
-            <h1 className='text-white'>Meter Widget</h1>
-        </div>
+        <Alert>You added Me!!</Alert>
     );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   mode: "jit",
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+      "./index.html", "./src/**/*.{js,ts,jsx,tsx}",
+      'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
     extend: {
       colors: {
-        primary : "#4936D2",
-        secondary : "#1D212D",
-        background : "#181824",
-        text : "#FFFFFF",
-      }
+        primary: "#4936D2",
+        secondary: "#1D212D",
+        background: "#181824",
+        text: "#FFFFFF",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+      require("flowbite/plugin")({
+        charts: true,
+      })
+  ],
 };
