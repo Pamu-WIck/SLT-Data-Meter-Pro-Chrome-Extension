d41021866053b7b3e69b9ff4dc81040a3869f352
Pamu_Wick <pamudithawm@gmail.com>
2023-11-07 22:32:50 +0530

    add transition to widgets

--- src/MeterWidget.tsx
+++ src/MeterWidget.tsx
@@ -1,12 +1,11 @@
-
-const MeterWidget = ({percentage}: {percentage: string}) => {
+const MeterWidget = ({percentage}: { percentage: string }) => {
     // const percentage = "80%";
     return (
-        <div className="flex-col rounded-md px-8 py-3 text-white bg-secondary felx">
+        <div className="flex-col rounded-md px-8 py-3 text-white bg-secondary felx hover:shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] hover:scale-105 transition-all duration-300">
             <div className="flex flex-row place-content-between">
-                <p>2.7 GB</p>
-                <p className="font-roboto font-semibold">Standard Data</p>
-                <p>37.3 GB</p>
+                <p className="font-medium">2.7 GB</p>
+                <p className="font-semibold opacity-80 font-roboto">Standard Data</p>
+                <p className="font-medium">37.3 GB</p>
             </div>
             <div className="mx-auto rounded py-1 text-center w-100 bg-secondary">
                 <div className="h-6 w-full rounded-full bg-opacity-30 bg-primary_purple">
@@ -16,9 +15,9 @@ const MeterWidget = ({percentage}: {percentage: string}) => {
                 </div>
             </div>
             <div className="flex flex-row place-content-between">
-                <div className="average-usage bg-primary_purple">2.8 GB</div>
-                <div className="average-usage bg-primary_green">2.8 GB</div>
-                <div className="average-usage bg-primary_purple">2.8 GB</div>
+                <div className="bg-opacity-50 average-usage bg-primary_purple">2.8 GB</div>
+                <div className="bg-opacity-50 average-usage bg-primary_green">2.8 GB</div>
+                <div className="bg-opacity-50 average-usage bg-primary_purple">2.8 GB</div>
             </div>
         </div>
     )
