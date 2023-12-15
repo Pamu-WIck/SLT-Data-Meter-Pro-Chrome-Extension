b693bc7aa46483bb6f23648560f4c6613f11f912
Pamu_Wick <pamudithawm@gmail.com>
2023-12-15 23:20:19 +0530

    login function

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./styles/App.css";
 import Login from "./components/Login.tsx";
-
+import MainPanel from "./components/MainPanel.tsx";
 
 function App() {
+
+    const token = localStorage.getItem("token");
+    // console.log(token);
 
   return (
     <>
       <div className="w-max min-w-[500px] bg-background" >
-          <Login/>
+            {token ? <MainPanel /> : <Login />}
       </div>
     </>
   );
