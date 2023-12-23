f2d84e1e91cf241a82b2b28fcbe8db534b5e93a5
Pamu_Wick <pamudithawm@gmail.com>
2023-12-23 21:44:04 +0530

    add lazyload to chart

--- src/components/MainPanel.tsx
+++ src/components/MainPanel.tsx
@@ -1,9 +1,10 @@
 import MeterWidget from "./MeterWidget.tsx";
 import Header from "./Header.tsx";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import {fetchUsageSummary, fetchVas} from "../data/fetch.ts";
 import {Spinner} from "flowbite-react";
-import UsageChart from "./UsageChart.tsx";
+
+const UsageChart = React.lazy(() => import("./UsageChart.tsx"));
 
 const MainPanel = () => {
     const [usageData, setUsageData] = useState([] as any[]);
@@ -48,7 +49,9 @@ const MainPanel = () => {
                     <MeterWidget key={index} json={usageItem} />
                 ))}
 
-                {showUsageChart && <UsageChart/>}
+                <Suspense fallback={<div>Loading...</div>}>
+                    {showUsageChart && <UsageChart/>}
+                </Suspense>
 
             </div>
         </>
